0b9e529ec5e705c6ef46513cbd2ca7be29773f98
Kristina Pankova <kristinapankova@outlook.com>
2020-10-25 22:31:55 +0200

    Add smt)

--- Navg_Page/Navg_Page/esmaspaev.xaml.cs
+++ Navg_Page/Navg_Page/esmaspaev.xaml.cs
@@ -14,7 +14,24 @@ namespace Navg_Page
     {
         public esmaspaev()
         {
-            InitializeComponent();
+            string[] tasks = new string[] { "Просыпаюсь", "Завтракаю", "Еду на учебу", "Завершение учебного дня", "Еду домой", "Занимаюсь своими делами", "Сон" };
+            ListView list = new ListView();
+            Button back = new Button { Text = "Назад", BackgroundColor = Color.Wheat };
+            back.Clicked += Back_Clicked;
+
+            list.ItemsSource = tasks;
+            list.ItemSelected += List_ItemSelected;
+            Content = new StackLayout { Children = { list, back } };
+        }
+
+        private async void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
         }
     }
 }
